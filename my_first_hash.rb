d9e47a626c8da4bd568f30eba5e87b6906ee7f67
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-17 02:48:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,7 +37,7 @@ def adding
                   "oil paintings" => 3
                  }
                  
-  shipping_manifest["mustkets"] = 2 
+  shipping_manifest["muskets"] = 2 
   shipping_manifest["gun powder"] = 4
   
   return shipping_manifest
