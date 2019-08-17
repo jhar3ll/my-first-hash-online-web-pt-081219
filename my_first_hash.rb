d77be2f1a6d0a54b9bbaf622ce664569f84cbf9b
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-17 02:44:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,12 +7,17 @@ current_games = {
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+
+ the_manifest = {
+   "whale bone corsets" => 5,
+   "porcelain vases" => 2,
+   "oil paintings" => 3
+ }
+ end
 
 
-end
+
+
 
 def retrieval
   shipping_manifest = {
@@ -21,7 +26,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+shipping_manifest["oil paintings"]
 
 end
 
