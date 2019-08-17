340896a94304291eba78792fcaa82787873c4730
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-17 02:38:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,8 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+current_games = hash.new {
+  "xbox" => "rocket league"
+  
+}
   
 end
 
