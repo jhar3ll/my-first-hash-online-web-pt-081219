1513d97775e8927995006d452888a5f73724b756
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-17 02:45:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,14 +36,10 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+                 
+  shipping_manifest["mustkets"] = 2 
+  shipping_manifest["gun powser"] = 4
+end
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+ shipping_manifest
 end
